13333d22bcc0e76c148266fbb7695c3e647494c2
mikeascalon <mikeascalon@yahoo.com>
2024-01-29 16:49:42 -0800

    edited the capital6

--- api/index.py
+++ api/index.py
@@ -1,51 +1,40 @@
-# from http.server import BaseHTTPRequestHandler
-# from urllib import parse
-# import requests
-
-# class handler(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         s = self.path
-#         print(s)
-#         url_components = parse.urlsplit(s)
-#         query_string_list = parse.parse_qsl(url_components.query)
-#         dic = dict(query_string_list)
-
-#         country_name = dic.get("country_name")
-#         if country_name:  # Check if country_name is not None
-#             url = "https://restcountries.com/v3.1/name/"
-
-#             r = requests.get(url + country_name)
-        
-#             if r.status_code == 200:
-#                 data = r.json()
-#                 if isinstance(data, list) and len(data) > 0:
-#                     capital = data[0].get("capital")
-#                     if capital:
-#                         message = f"The capital of {country_name} is {capital}"
-#                     else:
-#                         message = f"No capital information found for {country_name}"
-#                 else:
-#                     message = f"Country {country_name} not found"
-#             else:
-#                 message = "Error fetching data from the API"
-#         else:
-#             message = "Give me a country_name to find the capital please"
-
-#         self.send_response(200)
-#         self.send_header('Content-type', 'text/plain')
-#         self.end_headers()
-
-#         self.wfile.write(message.encode())
-
-#         return
-
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # s = self.path
+        # print(s)
+        # url_components = parse.urlsplit(s)
+        # query_string_list = parse.parse_qsl(url_components.query)
+        # dic = dict(query_string_list)
+
+        # country_name = dic.get("country_name")
+        # capital = dic.get("capital")
+        # if country_name in dic:  # Corrected the key to "country_name"
+        #     url = "https://restcountries.com/v3.1/name/"
+        #     # country_name = dic[country_name]
+
+        #     r = requests.get(url + country_name)
+        
+        #     data = r.json()
+        #     # informations = []
+
+        #     # for countries in data:
+        #     #     if "capital" in countries:  # Check if "capital" key exists
+        #     #         capital = countries["capital"]
+        #     #         informations.append(capital)
+        #     # message = str(informations)
+        #     message = "country found"
+
+        # else:
+        #     message = "Give me a country to find the capital please"
+        message = "country found"
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = "Howdy"
+
         self.wfile.write(message.encode())
-        return
+
+        return 
