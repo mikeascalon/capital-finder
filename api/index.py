fbc73c327646e4a17685b79d3467fe6f24564a90
mikeascalon <mikeascalon@yahoo.com>
2024-01-29 17:02:37 -0800

    edited the capital7

--- api/index.py
+++ api/index.py
@@ -4,21 +4,25 @@ import requests
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # s = self.path
-        # print(s)
-        # url_components = parse.urlsplit(s)
-        # query_string_list = parse.parse_qsl(url_components.query)
-        # dic = dict(query_string_list)
+        s = self.path
+       
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
 
-        # country_name = dic.get("country_name")
-        # capital = dic.get("capital")
+        country_name = dic.get("country")
+        capital = dic.get("capital")
         # if country_name in dic:  # Corrected the key to "country_name"
-        #     url = "https://restcountries.com/v3.1/name/"
+        if country_name:
+            url = f"https://restcountries.com/v3.1/name/{country_name}"
         #     # country_name = dic[country_name]
 
         #     r = requests.get(url + country_name)
-        
-        #     data = r.json()
+            r = requests.get(url)
+            data = r.json()
+            capital_info = data[0]["capital"]
+            join_capitals = " and ".join(capital_info)
+            message = f"the capital of {country_name} is {join_capitals}"
         #     # informations = []
 
         #     # for countries in data:
@@ -30,7 +34,7 @@ class handler(BaseHTTPRequestHandler):
 
         # else:
         #     message = "Give me a country to find the capital please"
-        message = "country found"
+        # message = "country found"
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
