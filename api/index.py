4e2adcde0203a3313207a5bc31bca046f0a65cdb
mikeascalon <mikeascalon@yahoo.com>
2024-01-29 16:38:53 -0800

    edited the capital3

--- api/index.py
+++ api/index.py
@@ -10,20 +10,23 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        if "country_name" in dic:  # Corrected the key to "country_name"
+        country_name = dic.get("country_name")
+        capital = dic.get("capital")
+        if country_name in dic:  # Corrected the key to "country_name"
             url = "https://restcountries.com/v3.1/name/"
-            country_name = dic["country_name"]
+            # country_name = dic[country_name]
 
             r = requests.get(url + country_name)
         
             data = r.json()
-            informations = []
+            # informations = []
 
-            for countries in data:
-                if "capital" in countries:  # Check if "capital" key exists
-                    capital = countries["capital"]
-                    informations.append(capital)
-            message = str(informations)
+            # for countries in data:
+            #     if "capital" in countries:  # Check if "capital" key exists
+            #         capital = countries["capital"]
+            #         informations.append(capital)
+            # message = str(informations)
+            message = "country found"
 
         else:
             message = "Give me a country to find the capital please"
